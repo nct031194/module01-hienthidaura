b1c7bd2879f4a4ecdcec830b48b0baef5c195c87
nct031194 <nct031194@icloud.com>
2023-03-06 16:22:47 +0700

    add hienthidaura.html and .js file

---
+++ hienthidaura.js
@@ -0,0 +1,12 @@
+function showMessage(){
+    alert('Chào mừng đến với bình nguyên zô tận')
+}
+function showMessage2(){
+    document.getElementById('messageDiv').innerHTML = 'Nguyễn Chí Thành';
+}
+function showMessage3(){
+    document.write('Chào bạn nha^^');
+}
+function showMessage4(){
+    console.log('Chào nha');
+}
